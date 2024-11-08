b550d747bf83736b8a43fcaa1839d2b3e7ae5938
Luan <132374218+Luanhts@users.noreply.github.com>
2024-11-08 18:37:08 -0300

    sem .env

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,7 +1,7 @@
 import express from "express";
 const mainRouter = express.Router();
 
-import usuarioRouter from "./usuarioRouter.js";
+import usuarioRouter from "./convertRouter.js";
 mainRouter.use('/usuario', usuarioRouter)
 
 import convertRouter from "./convertRouter.js";
