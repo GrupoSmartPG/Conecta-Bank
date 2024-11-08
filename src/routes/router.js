4e50aa1aafd51a28bb19cba708974acfd92d928f
MorcegodeObra <geefoliveira@gmail.com>
2024-11-08 16:44:30 -0300

    DEU BOA AS TRANSFER

--- src/routes/router.js
+++ src/routes/router.js
@@ -7,4 +7,7 @@ mainRouter.use('/usuario', usuarioRouter)
 import carteiraRouter from "./carteiraRouter.js"
 mainRouter.use('/carteira', carteiraRouter)
 
+import transacaoRouter from "./transacoesRouter.js";
+mainRouter.use('/transacao', transacaoRouter)
+
 export default mainRouter;
