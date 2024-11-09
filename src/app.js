5f97ba6406bbc0d4b526eb04078bf31e845e3ab9
SauvviTech <fpedrapaixao@gmail.com>
2024-11-09 17:32:54 -0300

    remove cors

--- src/app.js
+++ src/app.js
@@ -1,46 +1,20 @@
 import express from "express";
-import cors from "cors";
-import connectDB from "./config/db.js";
-import mainRouter from "./routes/router.js";
-
-// Criação do servidor Express
 const app = express();
-
-// Configuração do CORS
-app.use(cors({
-  origin: (origin, callback) => {
-    // Permitir todas as origens
-    callback(null, true);
-  },
-  credentials: true, // Permitir envio de cookies e credenciais
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos HTTP permitidos
-  allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
-}));
-
-// Middleware para lidar com pré-requisições
-app.options('*', cors()); // Lidar com preflight requests
-
-// Middleware para interpretar JSON
 app.use(express.json());
+import connectDB from "./config/db.js";
 
-// Conexão com o banco de dados
 connectDB()
-  .then(() => console.log("Conectado ao banco de dados"))
-  .catch((err) => {
-    console.error("Erro ao conectar ao banco de dados:", err.message);
-    process.exit(1); // Finaliza o processo em caso de erro crítico
-  });
 
-// Roteador principal
-app.use("/", mainRouter);
+import mainRouter from "./routes/router.js";
+app.use('/', mainRouter)
 
-// Rota para verificar se o servidor está funcionando
-app.get("/", (req, res) => {
-  res.status(200).send("Servidor ok!");
-});
 
-// Inicialização do servidor
+
+app.get("/",(req,res)=>{
+    res.status(200).send("Servidor ok!")
+})
+
 const porta = 3000;
-app.listen(porta, () => {
-  console.log(`Servidor rodando na porta ${porta}`);
-});
+app.listen(porta,() =>{
+    console.log(`Servidor rodando na porta ${porta}`);
+})
