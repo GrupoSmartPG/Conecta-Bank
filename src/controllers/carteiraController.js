9d3a51bfa96320ceb8c830c788addfcc52de5c64
Marcos Felipe Conrado <marcosconrado007@gmail.com>
2024-11-09 13:13:23 -0300

    Update carteiraController.js

--- src/controllers/carteiraController.js
+++ src/controllers/carteiraController.js
@@ -1,8 +1,22 @@
 import Carteira from '../models/carteiraModel.js';
+import Usuario from '../models/usuarioModel.js';
 
 export const store = async (req, res) => {
     try {
-        const carteira = await Carteira.create(req.body);
+        const user = await Usuario.findOne({ email: req.body.email });
+        if(!user){
+            return res.status(500).json('Usuário não encontrado.');
+        };
+        if(!req.body.tipoMoeda){
+            tipo = 'Real';
+        } else{
+            tipo = req.body.tipoMoeda;
+        };
+        const carteira = {
+            usrId:user._id,
+            tipoMoeda: tipo
+        };
+        await Carteira.create(carteira);
         return res.status(201).json(carteira);
     } catch (error) {
         return res.status(500).json({ error: error });
